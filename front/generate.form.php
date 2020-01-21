62422b4a8e2f19143e90d70f728250eefa99f379
Mateusz Nitka <mateusznitka01@gmail.com>
2020-01-21 16:03:43 +0100

    Code clean

--- front/generate.form.php
+++ front/generate.form.php
@@ -12,6 +12,4 @@ if (isset($_REQUEST['delete'])) {
 	Html::back();
 }
 
-/*$_POST['number'], $_POST['type_name'], $_POST['man_name'], $_POST['mod_name'], $_POST['serial'], $_POST['item_name'], $_POST['owner'], $_POST['user_id'], $_POST['author'], $_POST['list']*/
-
 ?>
